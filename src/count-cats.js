35c3ec26bd72a3b0c0cb9d9e4b4a0a2f60b6c2bb
alpoliakov <alpoliakov73@gmail.com>
2020-09-03 03:19:55 +0300

    feat: Added some solutions

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
   // remove line with error and write your code here
+  let count = 0;
+  if(!Array.isArray(matrix) || matrix.length === 0) return count;
+  for(let i = 0; i <= matrix.length; i++) {
+    if(!Array.isArray(matrix[i]) && matrix[i] !== '^^') {
+      continue;
+    }else if(matrix[i] === '^^') {
+      count =+ 1;
+      continue;
+    }else if (Array.isArray(matrix[i])) {
+      matrix[i].forEach(item => {
+        if(item === '^^') {
+          count += 1
+        }
+      })
+    }
+  }
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam( members ) {
   // remove line with error and write your code here
+  if(!Array.isArray(members) || members.length === 0) return false;
+  return members.filter(item => (typeof item === 'string' && item.length !== 0)).map(item => {
+    return item.replace(/\s/g, '')
+  }).map(item => item[0].toUpperCase()).sort().join('');
 };
